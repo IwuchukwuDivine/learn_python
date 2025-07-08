bd17e325a2cd9a4febef985ac6ec0d3a910d963a
Iwuchukwu Divine Ifunanya <ekeneifunanya@gmail.com>
2025-07-08 07:55:18 +0100

    Implemented price correction for transactions in Excel and added a bar chart visualization. Removed weight converter and guessing game functionalities from app.py.

--- app.py
+++ app.py
@@ -1,28 +1,47 @@
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
 
 wb = xl.load_workbook("transactions.xlsx")
 sheet = wb["Sheet1"]
 
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    if isinstance(cell.value, (int, float)):
+        corrected_price = cell.value * 0.9
+        sheet.cell(row, 4).value = corrected_price
 
-# weight converter
-weight = int(input("weight: "))
-unit = input("(L)bs or (K)g: ")
+values = Reference(
+    sheet,
+    min_row=2,
+    max_row=sheet.max_row,
+    min_col=4,
+    max_col=4
+)
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, "F2")
 
-if unit.lower() == "l":
-    converted = round(weight * 0.45)
-    print(f"you are {converted} kilos")
-else:
-    converted = round(weight / 0.45)
-    print(f"you are {converted} pounds")
+wb.save("transactions2.xlsx")
 
-# guessing game with max of 4 trials
-num_of_trials = 0
+# # weight converter
+# weight = int(input("weight: "))
+# unit = input("(L)bs or (K)g: ")
 
-while num_of_trials < 4:
-    guess = int(input("guess: "))
-    if guess == 9:
-        print("you won!")
-        break
-    num_of_trials += 1
-else:
-    print("you lost!")
+# if unit.lower() == "l":
+#     converted = round(weight * 0.45)
+#     print(f"you are {converted} kilos")
+# else:
+#     converted = round(weight / 0.45)
+#     print(f"you are {converted} pounds")
+
+# # guessing game with max of 4 trials
+# num_of_trials = 0
+
+# while num_of_trials < 4:
+#     guess = int(input("guess: "))
+#     if guess == 9:
+#         print("you won!")
+#         break
+#     num_of_trials += 1
+# else:
+#     print("you lost!")
